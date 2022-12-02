2fead96b2dedb2d6146a91aa2738e12b66c4a0fd
griffin <griffincolomer@gmail.com>
2022-12-02 17:28:38 -0600

    added in the algorithm

--- MaximumCoverage.py
+++ MaximumCoverage.py
@@ -22,13 +22,27 @@ def parseFile(name):
     collection["U"] = set([x for x in range(1, int(n) + 1)])
     return int(m), int(k), collection
 
+def nextSet(collection, m, elementsDiscovered):
+    # helper function of maximumCoverageGreedy that that finds the next largest set
+    largest = set()
+    for i in range(1, m+1):
+        collection[i].difference_update(elementsDiscovered)
+    for i in range(1, m+1):
+        if len(collection[i]) > len(largest):
+            largest = collection[i]
+    return largest
 
-def maximumCoverage(collection, k, m):
-    # algorithm - currently just takes the smallest number from each set and puts them in a new set for testing purposes
-    ret = set()
-    for i in range(1, m + 1):
-        ret.add(min(collection[i]))
-    return f"min({k}, {m}): {ret}"
+
+def maximumCoverageGreedy(collection, k, m):
+    # algorithm - Uses a greedy method to find the largest set made by a collection of sets
+    elementsDiscovered = set()
+    output = set()
+    for i in range(k):
+        bigSet = nextSet(collection, m, elementsDiscovered)
+        elementsDiscovered = elementsDiscovered.union(bigSet)
+        output = output.union(bigSet)
+
+    return f"min({k}, {m}): {output}"
 
 
 def writeFile(name, content):
@@ -39,15 +53,13 @@ def writeFile(name, content):
 
 
 path = input("Enter the path to instance.txt files (more info in README)\n")
-
 try: os.mkdir("solutions")
 except: pass
 
 iteration = 1
-
 while os.path.exists(path + f"/instance{str(iteration).zfill(2)}.txt"):
     # m is the number of sets in the file, number k for the algorithm
     m, k, collection = parseFile(path + f"/instance{str(iteration).zfill(2)}.txt")
-    writeFile("solutions/" + f"solution{str(iteration).zfill(2)}.txt", maximumCoverage(collection, k, m))
+    writeFile("solutions/" + f"solution{str(iteration).zfill(2)}.txt", maximumCoverageGreedy(collection, k, m))
     print(collection)
     iteration += 1
